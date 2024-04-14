9b0b435242f9d369611e827a1781a2f714b9aebe
rvalladares77 <bobyvalla@gmail.com>
2024-04-13 21:31:17 -0600

    Add has many association for work logs

--- app/models/job_group.rb
+++ app/models/job_group.rb
@@ -1,5 +1,6 @@
 class JobGroup < ApplicationRecord
   has_many :employees
+  has_many :work_logs
   
   validates :name, presence: true, uniqueness: true
   validates :hourly_rate, presence: true, numericality: { greater_than: 0 }
