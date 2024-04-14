c2db288349c83bd523688c84f2ca00e58eac7e11
rvalladares77 <bobyvalla@gmail.com>
2024-04-13 20:17:57 -0600

    Refactoring

--- app/services/work_log_importer.rb
+++ app/services/work_log_importer.rb
@@ -12,13 +12,9 @@ class WorkLogImporter
 
 
   def create
-    pp invalid_import?
-    return error_message if invalid_import?
+    return [] if invalid_import?
 
     create_work_log
-
-
-    success_message
   end
 
 
@@ -27,23 +23,21 @@ class WorkLogImporter
     def create_work_log
       import = CsvImport.create(filename: file.original_filename, report_id: report_id)
       work_logs = []
-
+      errors = []
 
       CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
         employee = fetch_employee(row[:employee_id], row[:job_group])
-        
-        next if employee.nil?
 
-        work_logs << {
-          employee_id:   employee.id,
-          work_date:     row[:date],
-          hours_worked:  row[:hours_worked],
-          csv_import_id: import.id
-        }
-
+        if employee.present?
+          work_logs << work_log_attributes(employee, import, row)
+        else
+          errors << row[:employee_id]
+        end
       end
 
       WorkLog.insert_all(work_logs)
+
+      errors
     end
 
     def fetch_employee(employee_id, job_group)
@@ -64,11 +58,13 @@ class WorkLogImporter
       file.original_filename.match(/\d+/).to_s.to_i
     end
 
-    def error_message
-      [{ error: 'File with the same report ID already exists' }, :unprocessable_entity]
-    end
+    def work_log_attributes(employee, import, row)
+      {
+        employee_id:   employee.id,
+        work_date:     row[:date],
+        hours_worked:  row[:hours_worked],
+        csv_import_id: import.id
+      }
 
-    def success_message
-      [{ message: 'The CSV was uploaded successfully' }, :created]
     end
 end
