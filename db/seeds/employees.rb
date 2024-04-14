02e251ef16a455686dfc6b9b45481803a632fa4d
rvalladares77 <bobyvalla@gmail.com>
2024-04-14 08:40:21 -0600

    remove the suffix id

--- db/seeds/employees.rb
+++ db/seeds/employees.rb
@@ -1,4 +1,4 @@
-Employee.create(employee_id: '1', job_group_id: JobGroup.find_by(name: 'A'))
-Employee.create(employee_id: '2', job_group_id: JobGroup.find_by(name: 'B'))
-Employee.create(employee_id: '3', job_group_id: JobGroup.find_by(name: 'A'))
-Employee.create(employee_id: '4', job_group_id: JobGroup.find_by(name: 'B'))
+Employee.create(employee_id: '1', job_group: JobGroup.find_by(name: 'A'))
+Employee.create(employee_id: '2', job_group: JobGroup.find_by(name: 'B'))
+Employee.create(employee_id: '3', job_group: JobGroup.find_by(name: 'A'))
+Employee.create(employee_id: '4', job_group: JobGroup.find_by(name: 'B'))
