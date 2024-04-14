199e3298b39d91c7ca3191a7c87a9fdf6368755a
rvalladares77 <bobyvalla@gmail.com>
2024-04-13 21:17:58 -0600

    Add conditional json response as the WorkLogImporter returns an arrays of errors

--- app/controllers/api/v1/csv_imports_controller.rb
+++ app/controllers/api/v1/csv_imports_controller.rb
@@ -1,17 +1,23 @@
 class Api::V1::CsvImportsController < ApplicationController
+
   def create
-    work_log_importer = WorkLogImporter.new(params[:file])
-    response, status = work_log_importer.create
-
-
-    render json: response, status: status
+    errors = WorkLogImporter.new(params[:file]).create
+    
+    if errors.empty?
+      render succesfull_message
+    else
+      render error_message(errors)
+    end
   end
 
   private
 
 
-    def csv_import_params
+    def succesfull_message
+      { json: { message: 'File uploaded successfully'} , status: :created }
+    end
 
-
+    def error_message(error)
+      { json: { error: 'Some employees were not found', not_found_employees: error}, status: :unprocessable_entity }
     end
 end
