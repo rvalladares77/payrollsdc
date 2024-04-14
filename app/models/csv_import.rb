a89ec48d43592bbead2a049c408b78e080ed3076
rvalladares77 <bobyvalla@gmail.com>
2024-04-13 19:05:24 -0600

    Rename attribute from name to filename

--- app/models/csv_import.rb
+++ app/models/csv_import.rb
@@ -2,5 +2,5 @@ class CsvImport < ApplicationRecord
   has_many :work_logs
 
   validates :report_id, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true
+  validates :filename, presence: true, uniqueness: true
 end
